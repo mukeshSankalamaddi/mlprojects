bd7e7092db7b44185cde8b4a2dbc448bb4589e84
Mukesh <s.mukeshreddy2001@gmail.com>
2025-09-21 11:37:06 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -46,5 +47,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param,cv=3,n_jobs=3,ver
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
